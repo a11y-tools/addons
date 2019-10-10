284ef8e3c9648e3ab8e409827d9a14abb7758e3a
Nicholas Hoyt <nhoyt@illinois.edu>
2019-10-10 13:59:29 -0500

    Add use of content script for traversal add-on

--- traversal/sidebar/panel.js
+++ traversal/sidebar/panel.js
@@ -1,34 +1,71 @@
 var myWindowId;
 
 /*
-*   Update the sidebar's content:
-*   1) Get the active tab in this sidebar's window
-*   2) Display the active tab's URL
+*   When the sidebar loads, store the ID of its window, and update its content.
 */
-function updateContent() {
-  browser.tabs.query({windowId: myWindowId, active: true})
-    .then((tabs) => {
-      document.getElementById('content').textContent = tabs[0].url;
-    });
+browser.windows.getCurrent({ populate: true }).then( (windowInfo) => {
+  myWindowId = windowInfo.id;
+  getContent();
+});
+
+/*
+*   Update content when a new page is loaded into a tab.
+*/
+browser.tabs.onUpdated.addListener(getContent);
+
+/*
+*   Update content when a new tab becomes active.
+*/
+browser.tabs.onActivated.addListener(getContent);
+
+/*
+*   Listen for messages from the content script.
+*/
+browser.runtime.onMessage.addListener(
+  function (request, sender, sendResponse) {
+    updateSidebar(request);
+  }
+);
+
+/*
+*   getContent: Initiate processing that will return, via messaging, the
+*   current content information from the active tab.
+*/
+function getContent () {
+  let msg = "This extension only supports 'http' and 'https' protocols."
+  browser.tabs.query({ windowId: myWindowId, active: true })
+  .then((tabs) => {
+    let tab = tabs[0];
+    if (tab.url.indexOf('http:') != 0 && tab.url.indexOf('https:') != 0) {
+      updateSidebar (msg)
+    }
+    else {
+      browser.tabs.executeScript(null, { file: '../content.js' });
+    }
+  });
+}
+
+function getFormattedData (message) {
+  return `<h3>Title</h3>
+          <p>${message.title}</p>
+          <h3>URL</h3>
+          <p>${message.href}</p>`;
 }
 
 /*
-*   Update content when a new tab becomes active
+*   updateSidebar: Display the content sent by the content script.
 */
-browser.tabs.onActivated.addListener(updateContent);
+function updateSidebar (message) {
+  let content = '';
+  if (typeof message === 'object') {
+    content = getFormattedData(message);
+  }
+  else {
+    content = message;
+  }
 
-/*
-*   Update content when a new page is loaded into a tab
-*/
-browser.tabs.onUpdated.addListener(updateContent);
-
-/*
-*   When the sidebar loads, get the ID of its window, and update its content
-*/
-browser.windows.getCurrent({populate: true}).then((windowInfo) => {
-  myWindowId = windowInfo.id;
-  updateContent();
-});
+  document.getElementById('content').innerHTML = content;
+}
 
 /*
 *   Generic error handler
