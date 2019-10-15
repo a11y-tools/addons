21d1c97a2962ddd192e69d4a028681cfc9a501f0
Nicholas Hoyt <nhoyt@illinois.edu>
2019-10-15 17:42:44 -0500

    Update structure/background.js: centralize message handling

--- structure/background.js
+++ structure/background.js
@@ -2,7 +2,11 @@
 *   background.js
 */
 var sidebarIsOpen;
+var contentInfo;
 
+/*
+*   Toggle the sidebar when browserAction button is clicked
+*/
 browser.browserAction.onClicked.addListener(function (e) {
   if (sidebarIsOpen) {
     browser.sidebarAction.close();
@@ -11,3 +15,12 @@ browser.browserAction.onClicked.addListener(function (e) {
     browser.sidebarAction.open();
   }
 });
+
+/*
+*   Listen for messages from the content script
+*/
+browser.runtime.onMessage.addListener(
+  function (request, sender, sendResponse) {
+    contentInfo = request;
+  }
+);
