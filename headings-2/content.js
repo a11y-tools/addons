1c69821b1dbe007feacee20c8813b6c9c5fdd7fc
Nicholas Hoyt <nhoyt@illinois.edu>
2019-11-19 10:14:45 -0600

    Copy structure snapshot to headings-2

---
+++ headings-2/content.js
@@ -0,0 +1,125 @@
+/*
+*   content.js
+*/
+var headingRefs;
+
+browser.runtime.sendMessage({
+  infoList: getInfo(),
+  title: document.title,
+  url: window.location.href
+});
+
+browser.runtime.onMessage.addListener (
+  function (request, sender, sendResponse) {
+    if (request.id !== 'find') return;
+    let element = headingRefs[request.index];
+    if (isInPage(element)) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    }
+    else {
+      console.log('Element was removed from DOM: ' + element)
+    }
+});
+
+/*
+*   isInPage: This function checks to see if an element is a descendant of
+*   the page's body element. Because 'contains' is inclusive, isInPage returns
+*   false when the argument is the body element itself.
+*   MDN: https://developer.mozilla.org/en-US/docs/Web/API/Node/contains
+*/
+function isInPage (element) {
+  if (element === document.body) return false;
+  return document.body.contains(element);
+}
+
+/*
+*   isVisible: Recursively check element properties from getComputedStyle
+*   until document element is reached, to determine whether element or any
+*   of its ancestors has properties set that affect its visibility.
+*/
+function isVisible (element) {
+  if (element.nodeType === Node.DOCUMENT_NODE) return true;
+
+  if (element.nodeType === Node.ELEMENT_NODE) {
+    let computedStyle = window.getComputedStyle(element, null);
+    let display    = computedStyle.getPropertyValue('display');
+    let visibility = computedStyle.getPropertyValue('visibility');
+    let hidden     = element.getAttribute('hidden');
+    let ariaHidden = element.getAttribute('aria-hidden');
+
+    if ((display === 'none') || (visibility === 'hidden') ||
+        (hidden !== null) || (ariaHidden === 'true')) {
+      return false;
+    }
+  }
+  return isVisible(element.parentNode);
+}
+
+/*
+*   isHeading: Determine whether element is a heading based on its tagName.
+*/
+function isHeading (element) {
+  let headingNames = ['H1', 'H2', 'H3', 'H4', 'H5', 'H6'];
+  return (headingNames.indexOf(element.tagName) >= 0);
+}
+
+/*
+*   getDescendantTextContent: Collect the textContent values of child text
+*   nodes, and descendant text nodes of child elements that meet the predicate
+*   condition, by storing the values in the results array.
+*/
+function getDescendantTextContent (node, predicate, results) {
+  // Process the child nodes
+  for (let i = 0; i < node.childNodes.length; i++) {
+    let child = node.childNodes[i];
+
+    switch (child.nodeType) {
+      case (Node.ELEMENT_NODE):
+        if (predicate(child)) {
+          getDescendantTextContent(child, predicate, results);
+        }
+        break;
+      case (Node.TEXT_NODE):
+        let content = child.textContent.trim();
+        if (content.length) { results.push(content); }
+        break;
+    }
+  }
+}
+
+/*
+*   getInfo: Use the traverseDom function to recursively find each visible
+*   heading element in the document, collect its text content from visible
+*   child elements and store all relevant info in the returned infoList.
+*/
+function getInfo () {
+  let infoList = [];
+
+  // Reset headingRefs array
+  headingRefs = [];
+
+  function traverseDom (startElement) {
+    // Process the child elements
+    for (let i = 0; i < startElement.children.length; i++) {
+      let element = startElement.children[i];
+      // Save information if element meets criteria
+      if (isHeading(element) && isVisible(element)) {
+        let results = [];
+        getDescendantTextContent(element, isVisible, results);
+        let headingInfo = {
+          name: element.tagName,
+          text: results.length ? results.join(' ') : ''
+        }
+        infoList.push(headingInfo);
+        headingRefs.push(element);
+      }
+      else {
+        traverseDom(element);
+      }
+    }
+  }
+
+  // Start with body element and traverse entire DOM
+  traverseDom(document.body);
+  return infoList;
+}
---
+++ headings-2/listbox.js
@@ -0,0 +1,222 @@
+/*
+*   ListBox: Object that encapsulates the state and behavior of a listbox
+*   container with listitem elements in order to provide keyboard support
+*   as recommended by ARIA Authoring Practices.
+*
+*   Desired behavior:
+*   1. Handle up-arrow, down-arrow, left-arrow, right-arrow, home, end,
+*      page-up and page-down key presses to move focus among listItems.
+*   2. Handle return and space key presses to initiate the desired action
+*      associated with the selected listItem.
+*
+*   Functionality/methods:
+*   1. Initialize the DOM elements (the container and its children) with the
+*      proper ARIA roles and tabindex values.
+*   2. Create and assign necessary event handlers to DOM elements.
+*   2. Maintain state information needed for the event handlers.
+*/
+
+function ListBox (domNode) {
+  this.container = domNode;
+  this.listItems = [];
+  this.firstItem = null;
+  this.lastItem  = null;
+  this.increment = 8;
+
+  this.validate();
+  this.init();
+}
+
+//-------------------
+//     validate
+//-------------------
+
+ListBox.prototype.validate = function () {
+  let container = this.container;
+
+  function containerIsDomElement () {
+    let msg = "The ListBox container is not a DOM Element";
+    if (!container instanceof Element) {
+      throw new TypeError(msg);
+    }
+  }
+
+  function containerHasChildElements () {
+    let msg = "The ListBox container has no child elements.";
+    if (container.childElementCount === 0) {
+      throw new Error(msg)
+    }
+  }
+
+  containerIsDomElement();
+  containerHasChildElements();
+};
+
+//-------------------
+//       init
+//-------------------
+
+ListBox.prototype.init = function () {
+  // Set ARIA role for listbox container
+  this.container.setAttribute('role', 'listbox');
+
+  // Configure each listItem and store it in listItems array
+  let children = this.container.children;
+
+  for (let i = 0; i < children.length; i++) {
+    let listItem = children[i];
+    this.configure(listItem);
+    this.listItems.push(listItem);
+  }
+
+  // Assign firstItem and lastItem
+  this.firstItem = this.listItems[0];
+  this.lastItem  = this.listItems[this.listItems.length - 1];
+
+  // Handle keydown events when container has focus
+  this.container.addEventListener('keydown', this.handleContainerKeydown.bind(this));
+};
+
+//-------------------
+//     configure
+//-------------------
+
+ListBox.prototype.configure = function (listItem) {
+  // Set ARIA role and tabIndex
+  listItem.setAttribute('role', 'listitem');
+  listItem.tabIndex = -1;
+
+  // Assign event handlers
+  listItem.addEventListener('keydown', this.handleItemKeydown.bind(this));
+  // listItem.addEventListener('click', this.handleClick.bind(this));
+};
+
+//----------------------------
+//   handleContainerKeydown
+//----------------------------
+
+ListBox.prototype.handleContainerKeydown = function (event) {
+  let flag = false;
+
+  switch (event.key) {
+
+    case 'ArrowLeft':
+    case 'ArrowUp':
+      this.setFocusLastItem();
+      flag = true;
+      break;
+
+    case 'ArrowRight':
+    case 'ArrowDown':
+      this.setFocusFirstItem();
+      flag = true;
+      break;
+  }
+
+  if (flag) {
+    event.stopPropagation();
+    event.preventDefault();
+  }
+}
+
+//-----------------------
+//   handleItemKeydown
+//-----------------------
+
+ListBox.prototype.handleItemKeydown = function (event) {
+  let flag = false;
+
+  switch (event.key) {
+
+    case 'ArrowLeft':
+    case 'ArrowUp':
+      this.setFocusPrevItem(event.target);
+      flag = true;
+      break;
+
+    case 'ArrowRight':
+    case 'ArrowDown':
+      this.setFocusNextItem(event.target);
+      flag = true;
+      break;
+
+    case 'PageUp':
+      this.setFocusPrevPage(event.target);
+      flag = true;
+      break;
+
+    case 'PageDown':
+      this.setFocusNextPage(event.target);
+      flag = true;
+      break;
+
+    case 'Home':
+      this.setFocusFirstItem();
+      flag = true;
+      break;
+
+    case 'End':
+      this.setFocusLastItem();
+      flag = true;
+      break;
+  }
+
+  if (flag) {
+    event.stopPropagation();
+    event.preventDefault();
+  }
+};
+
+//----------------------
+//   setFocus methods
+//----------------------
+
+ListBox.prototype.setFocusFirstItem = function () {
+  this.firstItem.focus();
+};
+
+ListBox.prototype.setFocusLastItem = function () {
+  this.lastItem.focus();
+};
+
+ListBox.prototype.setFocusPrevItem = function (currentItem) {
+  if (currentItem === this.firstItem) return;
+
+  let index = this.listItems.indexOf(currentItem);
+  this.listItems[index - 1].focus();
+};
+
+ListBox.prototype.setFocusNextItem = function (currentItem) {
+  if (currentItem === this.lastItem) return;
+
+  let index = this.listItems.indexOf(currentItem);
+  this.listItems[index + 1].focus();
+};
+
+ListBox.prototype.setFocusPrevPage = function (currentItem) {
+  if (currentItem === this.firstItem) return;
+
+  let index = this.listItems.indexOf(currentItem);
+  let tgtIndex = index - this.increment;
+
+  if (tgtIndex < 0) {
+    this.firstItem.focus();
+  }
+  else {
+    this.listItems[tgtIndex].focus();
+  }
+}
+
+ListBox.prototype.setFocusNextPage = function (currentItem) {
+  if (currentItem === this.lastItem) return;
+
+  let index = this.listItems.indexOf(currentItem);
+  let tgtIndex = index + this.increment;
+
+  if (tgtIndex > this.listItems.length - 1) {
+    this.lastItem.focus();
+  }
+  else {
+    this.listItems[tgtIndex].focus();
+  }
+}
