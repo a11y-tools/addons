50f1a4dd642d61d31d404852a1d8a58127afe027
Nicholas Hoyt <nhoyt@illinois.edu>
2021-11-13 11:53:09 -0600

    Convert ListBox from fn. to class

--- structure-2/listbox.js
+++ structure-2/listbox.js
@@ -1,7 +1,7 @@
 /*
-*   ListBox: Object that encapsulates the state and behavior of a listbox
-*   container with option role elements in order to provide keyboard support
-*   as recommended by ARIA Authoring Practices.
+*   ListBox: Class that encapsulates the state and behavior of a listbox
+*   container with 'option' role elements. Provides keyboard support as
+*   recommended by ARIA Authoring Practices.
 *
 *   Desired behavior:
 *   1. Handle focus of the listbox container
@@ -20,301 +20,255 @@
 *      to reflect the listbox state.
 */
 
-export function ListBox (domNode, notifyFn) {
-  this.container      = domNode;
-  this.notifyFn       = notifyFn;
+export class ListBox {
+  constructor (domNode, notifyFn) {
+    this.container      = domNode;
+    this.notifyFn       = notifyFn;
 
-  this.optionsList    = [];
-  this.selectedOption = null;
-  this.firstOption    = null;
-  this.lastOption     = null;
-  this.increment      = 6;
+    this.optionsList    = [];
+    this.selectedOption = null;
+    this.firstOption    = null;
+    this.lastOption     = null;
+    this.increment      = 6;
 
-  this.validate();
-  this.init();
-}
+    this.validate();
+    this.init();
+  }
 
-//-------------------
-//     validate
-//-------------------
+  validate () {
+    let listBox = this.container;
 
-ListBox.prototype.validate = function () {
-  let listBox = this.container;
-
-  function listBoxIsDomElement () {
-    let msg = "The ListBox is not a DOM Element";
-    if (!listBox instanceof Element) {
-      throw new TypeError(msg);
+    function listBoxIsDomElement () {
+      let msg = "The ListBox is not a DOM Element";
+      if (!listBox instanceof Element) {
+        throw new TypeError(msg);
+      }
     }
-  }
 
-  function listBoxHasChildElements () {
-    let msg = "The ListBox has no child elements.";
-    if (listBox.childElementCount === 0) {
-      throw new Error(msg)
+    function listBoxHasChildElements () {
+      let msg = "The ListBox has no child elements.";
+      if (listBox.childElementCount === 0) {
+        throw new Error(msg)
+      }
     }
+
+    listBoxIsDomElement();
+    listBoxHasChildElements();
   }
 
-  listBoxIsDomElement();
-  listBoxHasChildElements();
-};
+  init () {
+    // Set ARIA attributes
+    this.container.setAttribute('role', 'listbox');
+    this.container.setAttribute('aria-activedescendant', '');
 
-//-------------------
-//       init
-//-------------------
+    // Configure each option and store it in optionsList array
+    let children = this.container.children;
 
-ListBox.prototype.init = function () {
-  // Set ARIA attributes
-  this.container.setAttribute('role', 'listbox');
-  this.container.setAttribute('aria-activedescendant', '');
+    for (let i = 0; i < children.length; i++) {
+      let option = children[i];
+      this.configure(option, i);
+      this.optionsList.push(option);
+    }
 
-  // Configure each option and store it in optionsList array
-  let children = this.container.children;
+    // Use optionsList to set firstOption and lastOption
+    let length = this.optionsList.length;
+    this.firstOption = this.optionsList[0];
+    this.lastOption  = this.optionsList[length - 1];
 
-  for (let i = 0; i < children.length; i++) {
-    let option = children[i];
-    this.configure(option, i);
-    this.optionsList.push(option);
+    this.assignEventHandlers();
   }
 
-  // Use optionsList to set firstOption and lastOption
-  let length = this.optionsList.length;
-  this.firstOption = this.optionsList[0];
-  this.lastOption  = this.optionsList[length - 1];
+  configure (option, i) {
+    let prefix = 'opt-';
 
-  this.assignEventHandlers();
-};
-
-//-------------------
-//     configure
-//-------------------
-
-ListBox.prototype.configure = function (option, i) {
-  let prefix = 'opt-';
-
-  // Set ARIA role and id
-  option.setAttribute('role', 'option');
-  option.setAttribute('id', prefix + i);
-};
-
-//-------------------------
-//   assignEventHandlers
-//-------------------------
-
-ListBox.prototype.assignEventHandlers = function () {
-  let listBox = this.container;
-
-  // Handle keydown events
-  listBox.addEventListener('focus', this.handleFocus.bind(this));
-  listBox.addEventListener('keydown', this.handleKeyDown.bind(this));
-  listBox.addEventListener('mousedown', this.handleMouseDown.bind(this));
-  listBox.addEventListener('dblclick', this.handleDblClick.bind(this));
-}
-
-//-------------------
-//    handleFocus
-//-------------------
-
-ListBox.prototype.handleFocus = function (event) {
-  if (this.selectedOption === null) {
-    this.setSelected(this.firstOption);
-  }
-  else {
-    this.setSelected(this.selectedOption);
+    // Set ARIA role and id
+    option.setAttribute('role', 'option');
+    option.setAttribute('id', prefix + i);
   }
 
-  // event.stopPropagation();
-  // event.preventDefault();
-}
+  assignEventHandlers () {
+    let listBox = this.container;
 
-//-------------------
-//   handleKeyDown
-//-------------------
-
-ListBox.prototype.handleKeyDown = function (event) {
-  let flag = false;
-
-  switch (event.key) {
-
-    // Navigation keys
-    case 'ArrowLeft':
-    case 'ArrowUp':
-      this.selectPrevOption();
-      flag = true;
-      break;
-
-    case 'ArrowRight':
-    case 'ArrowDown':
-      this.selectNextOption();
-      flag = true;
-      break;
-
-    case 'PageUp':
-      this.selectPrevPage();
-      flag = true;
-      break;
-
-    case 'PageDown':
-      this.selectNextPage();
-      flag = true;
-      break;
-
-    case 'Home':
-      this.selectFirstOption();
-      flag = true;
-      break;
-
-    case 'End':
-      this.selectLastOption();
-      flag = true;
-      break;
-
-    // Activation keys
-    case 'Enter':
-    case ' ':
-      this.activateSelection();
-      flag = true;
-      break;
+    // Handle events
+    listBox.addEventListener('focus', this.handleFocus.bind(this));
+    listBox.addEventListener('keydown', this.handleKeyDown.bind(this));
+    listBox.addEventListener('mousedown', this.handleMouseDown.bind(this));
+    listBox.addEventListener('dblclick', this.handleDblClick.bind(this));
   }
 
-  if (flag) {
-    event.stopPropagation();
-    event.preventDefault();
-  }
-};
-
-//---------------------
-//   handleMouseDown
-//---------------------
-
-ListBox.prototype.handleMouseDown = function (event) {
-  let parentElement = event.target.parentElement;
-
-  if (parentElement.getAttribute('role') === 'option') {
-    this.setSelected(parentElement);
+  handleFocus (event) {
+    if (this.selectedOption === null) {
+      this.setSelected(this.firstOption);
+    }
+    else {
+      this.setSelected(this.selectedOption);
+    }
 
     // event.stopPropagation();
     // event.preventDefault();
   }
-};
 
-//--------------------
-//   handleDblClick
-//--------------------
+  handleKeyDown (event) {
+    let flag = false;
 
-ListBox.prototype.handleDblClick = function (event) {
-  this.activateSelection();
+    switch (event.key) {
 
-  // event.stopPropagation();
-  // event.preventDefault();
-}
+      // Navigation keys
+      case 'ArrowLeft':
+      case 'ArrowUp':
+        this.selectPrevOption();
+        flag = true;
+        break;
 
-//-------------------
-//    setSelected
-//-------------------
+      case 'ArrowRight':
+      case 'ArrowDown':
+        this.selectNextOption();
+        flag = true;
+        break;
 
-ListBox.prototype.setSelected = function (option) {
-  if (this.selectedOption) {
-    this.selectedOption.removeAttribute('aria-selected')
-  }
+      case 'PageUp':
+        this.selectPrevPage();
+        flag = true;
+        break;
 
-  this.selectedOption = option;
-  option.setAttribute('aria-selected', 'true');
-  this.container.setAttribute('aria-activedescendant', option.id);
-  this.scrollSelectedOption();
-  this.notifyFn({
-    action: 'navigate',
-    index: this.optionsList.indexOf(this.selectedOption)
-  });
-}
+      case 'PageDown':
+        this.selectNextPage();
+        flag = true;
+        break;
 
-//--------------------------
-//   scrollSelectedOption
-//--------------------------
+      case 'Home':
+        this.selectFirstOption();
+        flag = true;
+        break;
 
-ListBox.prototype.scrollSelectedOption = function () {
-  let listbox = this.container;
-  let element = this.selectedOption;
+      case 'End':
+        this.selectLastOption();
+        flag = true;
+        break;
 
-  // Note: element.offsetTop is the number of pixels from the top of the
-  // closest relatively positioned parent element. Thus the CSS for the
-  // ListBox container element must specify 'position: relative'.
-
-  if (listbox.scrollHeight > listbox.clientHeight) {
-
-    let elementBottom = element.offsetTop + element.offsetHeight;
-    let scrollBottom = listbox.clientHeight + listbox.scrollTop;
-
-    if (elementBottom > scrollBottom) {
-      listbox.scrollTop = elementBottom - listbox.clientHeight;
+      // Activation keys
+      case 'Enter':
+      case ' ':
+        this.activateSelection();
+        flag = true;
+        break;
     }
-    else if (element.offsetTop < listbox.scrollTop) {
-      listbox.scrollTop = element.offsetTop;
+
+    if (flag) {
+      event.stopPropagation();
+      event.preventDefault();
     }
   }
+
+  handleMouseDown (event) {
+    let parentElement = event.target.parentElement;
+
+    if (parentElement.getAttribute('role') === 'option') {
+      this.setSelected(parentElement);
+
+      // event.stopPropagation();
+      // event.preventDefault();
+    }
+  }
+
+  handleDblClick (event) {
+    this.activateSelection();
+
+    // event.stopPropagation();
+    // event.preventDefault();
+  }
+
+  setSelected (option) {
+    if (this.selectedOption) {
+      this.selectedOption.removeAttribute('aria-selected')
+    }
+
+    this.selectedOption = option;
+    option.setAttribute('aria-selected', 'true');
+    this.container.setAttribute('aria-activedescendant', option.id);
+    this.scrollSelectedOption();
+    this.notifyFn({
+      action: 'navigate',
+      index: this.optionsList.indexOf(this.selectedOption)
+    });
+  }
+
+  scrollSelectedOption () {
+    let listbox = this.container;
+    let element = this.selectedOption;
+
+    // Note: element.offsetTop is the number of pixels from the top of the
+    // closest relatively positioned parent element. Thus the CSS for the
+    // ListBox container element must specify 'position: relative'.
+
+    if (listbox.scrollHeight > listbox.clientHeight) {
+
+      let elementBottom = element.offsetTop + element.offsetHeight;
+      let scrollBottom = listbox.clientHeight + listbox.scrollTop;
+
+      if (elementBottom > scrollBottom) {
+        listbox.scrollTop = elementBottom - listbox.clientHeight;
+      }
+      else if (element.offsetTop < listbox.scrollTop) {
+        listbox.scrollTop = element.offsetTop;
+      }
+    }
+  }
+
+  activateSelection () {
+    this.notifyFn({
+      action: 'activate',
+      index: this.optionsList.indexOf(this.selectedOption)
+    });
+  }
+
+  selectFirstOption () {
+    this.setSelected(this.firstOption);
+  }
+
+  selectLastOption () {
+    this.setSelected(this.lastOption);
+  }
+
+  selectPrevOption () {
+    if (this.selectedOption === this.firstOption) return;
+
+    let index = this.optionsList.indexOf(this.selectedOption);
+    this.setSelected(this.optionsList[index - 1]);
+  }
+
+  selectNextOption () {
+    if (this.selectedOption === this.lastOption) return;
+
+    let index = this.optionsList.indexOf(this.selectedOption);
+    this.setSelected(this.optionsList[index + 1]);
+  }
+
+  selectPrevPage () {
+    if (this.selectedOption === this.firstOption) return;
+
+    let index = this.optionsList.indexOf(this.selectedOption);
+    let tgtIndex = index - this.increment;
+
+    if (tgtIndex < 0) {
+      this.selectFirstOption();
+    }
+    else {
+      this.setSelected(this.optionsList[tgtIndex]);
+    }
+  }
+
+  selectNextPage () {
+    if (this.selectedOption === this.lastOption) return;
+
+    let index = this.optionsList.indexOf(this.selectedOption);
+    let tgtIndex = index + this.increment;
+
+    if (tgtIndex > this.optionsList.length - 1) {
+      this.selectLastOption();
+    }
+    else {
+      this.setSelected(this.optionsList[tgtIndex]);
+    }
+  }
 }
-
-//-----------------------
-//   activateSelection
-//-----------------------
-
-ListBox.prototype.activateSelection = function () {
-  this.notifyFn({
-    action: 'activate',
-    index: this.optionsList.indexOf(this.selectedOption)
-  });
-}
-
-//--------------------
-//   select methods
-//--------------------
-
-ListBox.prototype.selectFirstOption = function () {
-  this.setSelected(this.firstOption);
-};
-
-ListBox.prototype.selectLastOption = function () {
-  this.setSelected(this.lastOption);
-};
-
-ListBox.prototype.selectPrevOption = function () {
-  if (this.selectedOption === this.firstOption) return;
-
-  let index = this.optionsList.indexOf(this.selectedOption);
-  this.setSelected(this.optionsList[index - 1]);
-};
-
-ListBox.prototype.selectNextOption = function () {
-  if (this.selectedOption === this.lastOption) return;
-
-  let index = this.optionsList.indexOf(this.selectedOption);
-  this.setSelected(this.optionsList[index + 1]);
-};
-
-ListBox.prototype.selectPrevPage = function () {
-  if (this.selectedOption === this.firstOption) return;
-
-  let index = this.optionsList.indexOf(this.selectedOption);
-  let tgtIndex = index - this.increment;
-
-  if (tgtIndex < 0) {
-    this.selectFirstOption();
-  }
-  else {
-    this.setSelected(this.optionsList[tgtIndex]);
-  }
-};
-
-ListBox.prototype.selectNextPage = function () {
-  if (this.selectedOption === this.lastOption) return;
-
-  let index = this.optionsList.indexOf(this.selectedOption);
-  let tgtIndex = index + this.increment;
-
-  if (tgtIndex > this.optionsList.length - 1) {
-    this.selectLastOption();
-  }
-  else {
-    this.setSelected(this.optionsList[tgtIndex]);
-  }
-};
